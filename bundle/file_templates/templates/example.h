a20ffed495e77ba91e0db1351f8101aa568092e9
Piotr Rogoża <piotr.r.public@gmail.com>
2014-06-08 00:57:51 +0200

    Added file_templates: https://sites.google.com/site/abudden/contents/Vim-Scripts/file-templates

---
+++ bundle/file_templates/templates/example.h
@@ -0,0 +1,21 @@
+/* ===============================================================
+   == <+#PROJECT_TITLE# - #MODULE_TITLE#;R++++++++++++++++++++> ==
+   == Copyright Mickey Mouse Ltd <+$YEAR$;R+++++++++++++++++++> ==
+   =============================================================== */
+
+/**
+ * @file <+$FILENAME$+>
+ * <+#PROJECT_TITLE#+> <+#MODULE_TITLE#+> Declaration.
+ *
+ *
+ */
+
+#ifndef _<+$UBASENAME$+>_H
+#define _<+$UBASENAME$+>_H
+
+#include "Global.h"
+
+void <+$BASENAME$+>_Init(void);
+void <+$BASENAME$+>_Update(void);
+
+#endif /* NOT def _<+$UBASENAME$+>_H */
